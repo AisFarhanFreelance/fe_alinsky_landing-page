803e3baab61ea9b77692924c9a1f596ed80922a8
Aldi-H <aldihimawan88@gmail.com>
2025-05-12 14:58:06 +0700

    feat: update SolutionOverviewCard to include labels for each card

--- src/components/home/solution-overview/solution-overview-card.tsx
+++ src/components/home/solution-overview/solution-overview-card.tsx
@@ -11,21 +11,25 @@ const cardData = [
     src: Avatar2,
     bg: "bg-alinsky-orchid-pink",
     icon: <Hashtag color="#e91e63" />,
+    label: "Social Media Manager",
   },
   {
     src: Avatar3,
     bg: "bg-alinsky-pink-lavender",
     icon: <StatusUp color="#9c27b0" />,
+    label: "Analytics Expert",
   },
   {
     src: Avatar4,
     bg: "bg-alinsky-uranian-blue",
     icon: <StatusUp color="#03A9F4" />,
+    label: "Campaign Manager",
   },
   {
     src: Avatar5,
     bg: "bg-alinsky-tea-rose-red",
     icon: <StatusUp color="#F44336" />,
+    label: "Lead Tracker",
   },
 ];
 
@@ -33,13 +37,22 @@ const SolutionOverviewCard = () => {
   return (
     <div className="grid grid-cols-4 gap-12">
       {cardData.map((item, idx) => (
-        <div key={idx} className="relative w-fit">
-          <Image src={item.src} alt={`Avatar${idx + 2}`} fetchPriority="high" />
-          <div
-            className={`absolute -bottom-2 -right-2 rounded-full p-[21.5px] ${item.bg}`}
-          >
-            {item.icon}
+        <div key={idx} className="flex flex-col items-center">
+          <div className="relative w-fit">
+            <Image
+              src={item.src}
+              alt={`Avatar${idx + 2}`}
+              fetchPriority="high"
+            />
+            <div
+              className={`absolute -bottom-2 -right-2 rounded-full p-[21.5px] ${item.bg}`}
+            >
+              {item.icon}
+            </div>
           </div>
+          <span className="mt-4 font-satoshi font-medium text-base text-alinsky-onyx">
+            {item.label}
+          </span>
         </div>
       ))}
     </div>
