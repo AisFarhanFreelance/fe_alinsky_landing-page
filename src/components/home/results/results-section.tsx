8c94c346ee0df49e45a5873b8462fd0c07db1ef3
Aldi-H <aldihimawan88@gmail.com>
2025-05-12 20:06:51 +0700

    feat: add ResultsSection component with informational content

---
+++ src/components/home/results/results-section.tsx
@@ -0,0 +1,19 @@
+const ResultsSection = () => {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center">
+      <div className="space-y-12 lg:mx-auto lg:my-60">
+        <div className="text-center font-helvetica font-bold text-alinsky-midnight-blue uppercase lg:text-9xl">
+          <span>Did You Know?</span>
+        </div>
+        <div className="px-auto text-center font-satoshi text-xl text-alinsky-gray-600">
+          <span>
+            Without automation, the average marketing team loses 520 hours per
+            year to manual social media tasks.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResultsSection;
