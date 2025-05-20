6d97e25e0cb235c8ebd423528b3cec3a2942001a
Aldi-H <aldihimawan88@gmail.com>
2025-05-20 09:56:18 +0700

    feat: inline CatalogLinks component directly into Footer for improved structure and maintainability

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,6 @@
 import { Instagram, Whatsapp } from "iconsax-reactjs";
 
 import CallToAction from "../home/call-to-action/call-to-action";
-import CatalogLinks from "./catalog-links";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -15,7 +14,36 @@ const Footer = () => {
         <div className="grid grid-cols-2 gap-6 p-12 text-alinsky-white">
           <div className="space-y-8">
             <h1 className="font-helvetica text-xs uppercase">Catalog</h1>
-            <CatalogLinks />
+            <div className="flex flex-row justify-start space-x-8 font-satoshi text-[28px] capitalize">
+              <div className="flex flex-col space-y-3">
+                <span className="group mb-4 inline-block w-fit cursor-pointer">
+                  Home
+                  <div className="-mt-4 h-3 w-full origin-left scale-x-100 bg-alinsky-medium-slate-blue/50 transition-transform duration-300" />
+                </span>
+                <span className="group mb-4 inline-block w-fit cursor-pointer">
+                  Pricing
+                  <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
+                </span>
+                <span className="group mb-4 inline-block w-fit cursor-pointer">
+                  Products
+                  <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
+                </span>
+              </div>
+              <div className="flex flex-col space-y-3">
+                <span className="group mb-4 inline-block w-fit cursor-pointer">
+                  Contact Us
+                  <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
+                </span>
+                <span className="group mb-4 inline-block w-fit cursor-pointer">
+                  Tutorial
+                  <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
+                </span>
+                <span className="group mb-4 inline-block w-fit cursor-pointer">
+                  Blog
+                  <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
+                </span>
+              </div>
+            </div>
           </div>
           <div className="grid grid-cols-2 gap-6">
             <div className="space-y-8">
