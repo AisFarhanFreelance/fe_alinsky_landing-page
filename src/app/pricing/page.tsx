758951be5e12121c24d829630f195f4cb01c1561
Aldi-H <aldihimawan88@gmail.com>
2025-06-03 13:16:21 +0700

    feat: add AgencyServiceSection component and integrate it into the Pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,9 +1,11 @@
+import AgencyServiceSection from "@/components/pricing/agency-service/agency-service-section";
 import PricingPlanSection from "@/components/pricing/pricing-plan/pricing-plan-section";
 
 const Pricing = () => {
   return (
     <div className="min-h-screen items-center justify-items-center">
       <PricingPlanSection />
+      <AgencyServiceSection />
     </div>
   );
 };
