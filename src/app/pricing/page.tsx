b37018cc10b32bad092f242474dd258e7ff13fa1
Aldi-H <aldihimawan88@gmail.com>
2025-06-06 09:18:35 +0700

    feat: add PricingCallToAction component and update ServiceBlockSection layout

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,3 +1,4 @@
+import PricingCallToAction from "@/components/call-to-action/pricing-call-to-action";
 import AgencyServiceSection from "@/components/pricing/agency-service/agency-service-section";
 import PricingPlanSection from "@/components/pricing/pricing-plan/pricing-plan-section";
 import ServiceBlockSection from "@/components/pricing/service-block/service-block-section";
@@ -8,6 +9,10 @@ const Pricing = () => {
       <PricingPlanSection />
       <AgencyServiceSection />
       <ServiceBlockSection />
+
+      <div className="relative w-full">
+        <PricingCallToAction />
+      </div>
     </div>
   );
 };
