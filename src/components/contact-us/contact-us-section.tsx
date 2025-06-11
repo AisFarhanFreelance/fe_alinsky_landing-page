da7365779fdfc3a1556c09b0212e10d76accf991
Aldi-H <aldihimawan88@gmail.com>
2025-06-11 10:45:01 +0700

    refactor: update layout structure of ContactUsSection for improved responsiveness

--- src/components/contact-us/contact-us-section.tsx
+++ src/components/contact-us/contact-us-section.tsx
@@ -2,24 +2,28 @@ import { Button } from "../ui/button";
 
 const ContactUsSection = () => {
   return (
-    <div className="my-[72px] flex h-[274px] items-center justify-center overflow-hidden">
-      <div className=" flex flex-col items-center justify-center space-y-10 text-center">
-        <div className="font-satoshi text-5xl leading-[100%] font-medium text-alinsky-midnight-blue capitalize">
-          See Your Brand In Our Platform
-        </div>
-        <div className="flex flex-row items-center gap-x-5 font-helvetica uppercase">
-          <Button className="bg-alinsky-midnight-blue px-[22px] py-2 text-[15px] leading-[26px] font-normal tracking-[0.46px] text-alinsky-white hover:bg-alinsky-midnight-blue/90">
-            Reach Out Directly
-          </Button>
-          <span className="text-[15px] leading-[100%] font-bold italic">
-            or
-          </span>
-          <Button
-            variant="outline"
-            className="border-alinsky-midnight-blue px-[22px] py-2 text-[15px] leading-[26px] font-normal tracking-[0.46px] text-alinsky-midnight-blue"
-          >
-            Book A Free Audit
-          </Button>
+    <div className="flex justify-center">
+      <div className="w-full max-w-[1920px]">
+        <div className="mx-[104px] my-[72px] flex h-[274px] items-center justify-center overflow-hidden alinsky-3xl:mx-[344px]">
+          <div className=" flex flex-col items-center justify-center space-y-10 text-center">
+            <div className="font-satoshi text-5xl leading-[100%] font-medium text-alinsky-midnight-blue capitalize">
+              See Your Brand In Our Platform
+            </div>
+            <div className="flex flex-row items-center gap-x-5 font-helvetica uppercase">
+              <Button className="bg-alinsky-midnight-blue px-[22px] py-2 text-[15px] leading-[26px] font-normal tracking-[0.46px] text-alinsky-white hover:bg-alinsky-midnight-blue/90">
+                Reach Out Directly
+              </Button>
+              <span className="text-[15px] leading-[100%] font-bold italic">
+                or
+              </span>
+              <Button
+                variant="outline"
+                className="border-alinsky-midnight-blue px-[22px] py-2 text-[15px] leading-[26px] font-normal tracking-[0.46px] text-alinsky-midnight-blue"
+              >
+                Book A Free Audit
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
