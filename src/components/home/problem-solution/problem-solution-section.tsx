9c420c4ba43b1f672eb5dfc457dae5b2aa628cf3
Aldi-H <aldihimawan88@gmail.com>
2025-06-11 10:44:07 +0700

    refactor: reorganize layout of ProblemSolutionSection for improved structure and readability

--- src/components/home/problem-solution/problem-solution-section.tsx
+++ src/components/home/problem-solution/problem-solution-section.tsx
@@ -3,30 +3,34 @@ import ProblemSolutionCard from "./problem-solution-card";
 const ProblemSolutionSection = () => {
   return (
     <div className="flex  min-h-screen items-center justify-center rounded-t-4xl bg-alinsky-anti-flash-white">
-      <div className="mx-[104px] mt-14 mb-8 flex flex-col justify-center space-y-8">
-        <div className="space-y-[72px] text-alinsky-rich-black">
-          <div className="text-center font-helvetica text-5xl capitalize">
-            4 Hidden Costs Slowing Down Your Client social media Growth
+      <div className="flex justify-center">
+        <div className="w-full max-w-[1920px]">
+          <div className="mx-[104px] mt-14 mb-8 flex flex-col justify-center space-y-8 alinsky-3xl:mx-[344px] ">
+            <div className="space-y-[72px] text-alinsky-rich-black">
+              <div className="text-center font-helvetica text-5xl capitalize">
+                4 Hidden Costs Slowing Down Your Client social media Growth
+              </div>
+              <div>
+                <ProblemSolutionCard />
+              </div>
+            </div>
+            <div className="flex flex-col space-y-6 rounded-2xl py-6 font-satoshi text-alinsky-midnight-blue capitalize">
+              <span className="pt-16 text-9xl leading-[20%] font-bold">
+                &ldquo;
+              </span>
+              <span className="text-[32px] leading-[150%] italic">
+                If even one of these problems feels too real… <br /> It&apos;s
+                time to reflect&nbsp;:
+                <b>
+                  &nbsp; What if your social media didn’t need more effort, just
+                  better tools
+                </b>
+              </span>
+              <span className="self-end pt-10 text-9xl leading-[20%] font-bold">
+                &rdquo;
+              </span>
+            </div>
           </div>
-          <div>
-            <ProblemSolutionCard />
-          </div>
-        </div>
-        <div className="flex flex-col space-y-6 rounded-2xl py-6 font-satoshi text-alinsky-midnight-blue capitalize">
-          <span className="pt-16 text-9xl leading-[20%] font-bold">
-            &ldquo;
-          </span>
-          <span className="text-[32px] leading-[150%] italic">
-            If even one of these problems feels too real… <br /> It&apos;s time
-            to reflect&nbsp;:
-            <b>
-              &nbsp; What if your social media didn’t need more effort, just
-              better tools
-            </b>
-          </span>
-          <span className="self-end pt-10 text-9xl leading-[20%] font-bold">
-            &rdquo;
-          </span>
         </div>
       </div>
     </div>
