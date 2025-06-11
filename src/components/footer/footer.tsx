a3252104dce2e849ac48d787a6b0d990341bfda4
Aldi-H <aldihimawan88@gmail.com>
2025-06-11 10:41:36 +0700

    refactor: update layout of Footer and Navbar components for improved structure and styling

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,73 +4,79 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="relative mx-[104px] mb-6">
-      <div className="rounded-4xl bg-alinsky-rich-black pt-32">
-        <div className="grid grid-cols-2 gap-6 p-12 text-alinsky-white">
-          <div className="space-y-8">
-            <h1 className="font-helvetica text-xs uppercase">Catalog</h1>
-            <div className="flex flex-row justify-start space-x-8 font-satoshi text-[28px] capitalize">
-              <div className="flex flex-col space-y-3">
-                <span className="group mb-4 inline-block w-fit cursor-pointer">
-                  Home
-                  <div className="-mt-4 h-3 w-full origin-left scale-x-100 bg-alinsky-medium-slate-blue/50 transition-transform duration-300" />
-                </span>
-                <span className="group mb-4 inline-block w-fit cursor-pointer">
-                  Pricing
-                  <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
-                </span>
-                <span className="group mb-4 inline-block w-fit cursor-pointer">
-                  Products
-                  <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
-                </span>
+    <div className="flex justify-center">
+      <div className="w-full max-w-[1920px]">
+        <div className="relative mx-[104px] mb-6 alinsky-3xl:mx-[344px]">
+          <div className="rounded-4xl bg-alinsky-rich-black pt-32">
+            <div className="grid grid-cols-2 gap-6 p-12 text-alinsky-white">
+              <div className="space-y-8">
+                <h1 className="font-helvetica text-xs uppercase">Catalog</h1>
+                <div className="flex flex-row justify-start space-x-8 font-satoshi text-[28px] capitalize">
+                  <div className="flex flex-col space-y-3">
+                    <span className="group mb-4 inline-block w-fit cursor-pointer">
+                      Home
+                      <div className="-mt-4 h-3 w-full origin-left scale-x-100 bg-alinsky-medium-slate-blue/50 transition-transform duration-300" />
+                    </span>
+                    <span className="group mb-4 inline-block w-fit cursor-pointer">
+                      Pricing
+                      <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
+                    </span>
+                    <span className="group mb-4 inline-block w-fit cursor-pointer">
+                      Products
+                      <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
+                    </span>
+                  </div>
+                  <div className="flex flex-col space-y-3">
+                    <span className="group mb-4 inline-block w-fit cursor-pointer">
+                      Contact Us
+                      <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
+                    </span>
+                    <span className="group mb-4 inline-block w-fit cursor-pointer">
+                      Tutorial
+                      <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
+                    </span>
+                    <span className="group mb-4 inline-block w-fit cursor-pointer">
+                      Blog
+                      <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
+                    </span>
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-col space-y-3">
-                <span className="group mb-4 inline-block w-fit cursor-pointer">
-                  Contact Us
-                  <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
-                </span>
-                <span className="group mb-4 inline-block w-fit cursor-pointer">
-                  Tutorial
-                  <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
-                </span>
-                <span className="group mb-4 inline-block w-fit cursor-pointer">
-                  Blog
-                  <div className="-mt-4 h-3 w-full origin-left scale-x-0 bg-alinsky-medium-slate-blue/50 transition-transform duration-300 group-hover:scale-x-100" />
-                </span>
+              <div className="grid grid-cols-2 gap-6">
+                <div className="space-y-8">
+                  <h1 className="font-helvetica text-xs uppercase">
+                    Contact Us
+                  </h1>
+                  <p className="font-satoshi text-base">
+                    <a
+                      href="mailto:info@alinsky.com"
+                      className="hover:text-alinsky-silver"
+                    >
+                      info@alinsky.com
+                    </a>
+                  </p>
+                </div>
+                <div className="space-y-8 justify-self-end">
+                  <h1 className="font-helvetica text-xs uppercase">
+                    Connect With Us
+                  </h1>
+                  <div className="flex flex-row justify-end space-x-1">
+                    <span className="cursor-pointer transition-transform duration-200 hover:scale-110">
+                      <Instagram color="#ffffff" size="24" />
+                    </span>
+                    <span className="cursor-pointer transition-transform duration-200 hover:scale-110">
+                      <Whatsapp color="#ffffff" size="24" />
+                    </span>
+                  </div>
+                </div>
               </div>
+            </div>
+            <div className="flex flex-row justify-between px-12 py-4 text-xs text-alinsky-battleship-gray">
+              <div>&copy; {currentYear} — Copyright</div>
+              <div>Privacy</div>
+              <div>All right reserved</div>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-6">
-            <div className="space-y-8">
-              <h1 className="font-helvetica text-xs uppercase">Contact Us</h1>
-              <p className="font-satoshi text-base">
-                <a
-                  href="mailto:info@alinsky.com"
-                  className="hover:text-alinsky-silver"
-                >
-                  info@alinsky.com
-                </a>
-              </p>
-            </div>
-            <div className="space-y-8 justify-self-end">
-              <h1 className="font-helvetica text-xs uppercase">
-                Connect With Us
-              </h1>
-              <div className="flex flex-row justify-end space-x-1">
-                <span className="cursor-pointer transition-transform duration-200 hover:scale-110">
-                  <Instagram color="#ffffff" size="24" />
-                </span>
-                <span className="cursor-pointer transition-transform duration-200 hover:scale-110">
-                  <Whatsapp color="#ffffff" size="24" />
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-row justify-between px-12 py-4 text-xs text-alinsky-battleship-gray">
-          <div>&copy; {currentYear} — Copyright</div>
-          <div>Privacy</div>
-          <div>All right reserved</div>
         </div>
       </div>
     </div>
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,25 +5,29 @@ import Alinsky_Logo from "/public/assets/logo/alinsky-logo.svg";
 
 const Navbar = () => {
   return (
-    <nav className="relative isolate mx-8 my-2 overflow-hidden">
-      <div className="relative my-5 hidden items-center font-helvetica text-base leading-5 sm:flex">
-        <Link href="#">
-          <Image
-            src={Alinsky_Logo}
-            alt="Alinsky-Logo"
-            style={{ width: "auto", height: "auto" }}
-            className="rounded-2xl"
-          />
-        </Link>
+    <div className="flex justify-center">
+      <div className="w-full max-w-[1920px]">
+        <nav className="relative isolate mx-[104px] my-2 overflow-hidden alinsky-3xl:mx-[344px]">
+          <div className="relative my-5 hidden items-center font-helvetica text-base leading-5 sm:flex">
+            <Link href="#">
+              <Image
+                src={Alinsky_Logo}
+                alt="Alinsky-Logo"
+                style={{ width: "auto", height: "auto" }}
+                className="rounded-2xl"
+              />
+            </Link>
 
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 space-x-8 rounded-2xl bg-alinsky-seasalt p-4 text-alinsky-dim-gray capitalize drop-shadow-lg">
-          <Link href="#">Home</Link>
-          <Link href="#">Pricing</Link>
-          <Link href="#">Features</Link>
-          <Link href="#">Contact Us</Link>
-        </div>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 space-x-8 rounded-2xl bg-alinsky-seasalt p-4 text-alinsky-dim-gray capitalize drop-shadow-lg">
+              <Link href="#">Home</Link>
+              <Link href="#">Pricing</Link>
+              <Link href="#">Features</Link>
+              <Link href="#">Contact Us</Link>
+            </div>
+          </div>
+        </nav>
       </div>
-    </nav>
+    </div>
   );
 };
 
