39541a522f562a3b7c8374d063e153b810918bf1
Aldi-H <aldihimawan88@gmail.com>
2025-06-11 10:43:04 +0700

    refactor: enhance layout and styling of AgencyServiceSection, PricingPlanSection, and ServiceBlockSection for improved responsiveness

--- src/components/pricing/pricing-plan/pricing-plan-section.tsx
+++ src/components/pricing/pricing-plan/pricing-plan-section.tsx
@@ -2,7 +2,7 @@ import PricingPlanCard from "./pricing-plan-card";
 
 const PricingPlanSection = () => {
   return (
-    <div className="relative flex min-h-screen w-full flex-col items-center justify-center overflow-hidden pb-16">
+    <div className="lex relative min-h-screen w-full flex-col overflow-hidden pb-16">
       {/* <div
         className="absolute inset-0 top-0 left-[-35.538px] z-0"
         style={{
@@ -14,18 +14,22 @@ const PricingPlanSection = () => {
         }}
       /> */}
 
-      <div className="mx-[104px] mt-[108px] space-y-16">
-        <div className="space-y-2 text-center capitalize">
-          <div className="font-helvetica text-2xl leading-16 font-bold text-alinsky-rich-black lg:text-5xl">
-            Pick Your Growth Blueprint
+      <div className="flex justify-center">
+        <div className="w-full max-w-[1920px]">
+          <div className="mx-[104px] mt-[108px] space-y-16  alinsky-3xl:mx-[344px]">
+            <div className="space-y-2 text-center capitalize">
+              <div className="font-helvetica text-2xl leading-16 font-bold text-alinsky-rich-black lg:text-5xl">
+                Pick Your Growth Blueprint
+              </div>
+              <div className="font-satoshi text-sm font-medium text-alinsky-dim-gray lg:text-xl">
+                All Plans Include Our Core Platform.&nbsp;
+                <span className="italic">Upgrade Anytime</span>
+              </div>
+            </div>
+            <div>
+              <PricingPlanCard />
+            </div>
           </div>
-          <div className="font-satoshi text-sm font-medium text-alinsky-dim-gray lg:text-xl">
-            All Plans Include Our Core Platform.&nbsp;
-            <span className="italic">Upgrade Anytime</span>
-          </div>
-        </div>
-        <div>
-          <PricingPlanCard />
         </div>
       </div>
     </div>
--- src/components/pricing/service-block/service-block-section.tsx
+++ src/components/pricing/service-block/service-block-section.tsx
@@ -8,68 +8,72 @@ import Circullar from "/public/assets/images/additional/circullar.svg";
 
 const ServiceBlockSection = () => {
   return (
-    <div className="flex h-[400px] items-center justify-center">
-      <div
-        className="absolute z-0 h-[740px] w-[780px]"
-        style={{
-          borderRadius: "780px",
-          background:
-            "linear-gradient(0deg, rgba(27, 37, 108, 0.50) 0%, rgba(27, 37, 108, 0.50) 100%)",
-          filter: "blur(364.95px)",
-        }}
-      />
+    <div className="flex justify-center">
+      <div className="w-full max-w-[1920px]">
+        <div className="flex h-[400px] items-center justify-center">
+          <div
+            className="absolute z-0 h-[740px] w-[780px]"
+            style={{
+              borderRadius: "780px",
+              background:
+                "linear-gradient(0deg, rgba(27, 37, 108, 0.50) 0%, rgba(27, 37, 108, 0.50) 100%)",
+              filter: "blur(364.95px)",
+            }}
+          />
 
-      <div className="z-10 mx-[104px] grid h-[400px] w-full grid-cols-3 items-center justify-center gap-6">
-        <Card className="flex h-full flex-col justify-between bg-alinsky-midnight-blue text-alinsky-white">
-          <CardContent className="mx-10 my-[64px] flex flex-1 flex-col justify-between">
-            <div className="max-w-[253px] font-helvetica text-4xl leading-12 font-bold tracking-[-0.18px]">
-              Meta/Tiktok Ads
-            </div>
-            <div className="h-auto w-[280px]">
-              <Image src={Bars} alt="Bar Chart Image" />
-            </div>
-          </CardContent>
-        </Card>
+          <div className="z-10 mx-[104px] grid h-[400px] w-full grid-cols-3 items-center justify-center gap-6 alinsky-3xl:mx-[344px]">
+            <Card className="flex h-full flex-col justify-between bg-alinsky-midnight-blue text-alinsky-white">
+              <CardContent className="mx-10 my-[64px] flex flex-1 flex-col justify-between">
+                <div className="max-w-[253px] font-helvetica text-4xl leading-12 font-bold tracking-[-0.18px]">
+                  Meta/Tiktok Ads
+                </div>
+                <div className="h-auto w-[280px]">
+                  <Image src={Bars} alt="Bar Chart Image" />
+                </div>
+              </CardContent>
+            </Card>
 
-        <Card className="relative flex h-full flex-col justify-between overflow-hidden bg-alinsky-white">
-          <CardContent className="mx-10 my-[64px] flex flex-1 flex-col justify-between">
-            <div
-              className="max-w-[306px] self-stretch font-helvetica text-4xl leading-[60px] font-bold tracking-[-0.18px]"
-              style={{
-                backgroundImage:
-                  "linear-gradient(145deg, #151F68 0%, #442FB8 62.26%, #6963E5 79.77%, #FDD8D8 95.38%, #5B5A5A 106.88%)",
-                backgroundClip: "text",
-                WebkitBackgroundClip: "text",
-                WebkitTextFillColor: "transparent",
-              }}
-            >
-              Done-For-you Content
-            </div>
+            <Card className="relative flex h-full flex-col justify-between overflow-hidden bg-alinsky-white">
+              <CardContent className="mx-10 my-[64px] flex flex-1 flex-col justify-between">
+                <div
+                  className="max-w-[306px] self-stretch font-helvetica text-4xl leading-[60px] font-bold tracking-[-0.18px]"
+                  style={{
+                    backgroundImage:
+                      "linear-gradient(145deg, #151F68 0%, #442FB8 62.26%, #6963E5 79.77%, #FDD8D8 95.38%, #5B5A5A 106.88%)",
+                    backgroundClip: "text",
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                  }}
+                >
+                  Done-For-you Content
+                </div>
 
-            <div className="absolute -right-10 bottom-0">
-              <Image
-                src={Hand}
-                alt="3D Hand Image"
-                className="h-[260px] w-[260px]"
-              />
-            </div>
-          </CardContent>
-        </Card>
+                <div className="absolute -right-10 bottom-0">
+                  <Image
+                    src={Hand}
+                    alt="3D Hand Image"
+                    className="h-[260px] w-[260px]"
+                  />
+                </div>
+              </CardContent>
+            </Card>
 
-        <Card className="relative flex h-full flex-col justify-between overflow-hidden bg-alinsky-white text-alinsky-midnight-blue">
-          <CardContent className="mx-10 my-[64px] flex flex-1 flex-col justify-between">
-            <div className="max-w-[315px] font-helvetica text-4xl leading-[52px] font-bold tracking-[-0.18px]">
-              Monthly Strategy Session
-            </div>
-            <div className="absolute -right-20 -bottom-10">
-              <Image
-                src={Circullar}
-                alt="Circullar Chart Image"
-                className="h-[325px] w-[325px]"
-              />
-            </div>
-          </CardContent>
-        </Card>
+            <Card className="relative flex h-full flex-col justify-between overflow-hidden bg-alinsky-white text-alinsky-midnight-blue">
+              <CardContent className="mx-10 my-[64px] flex flex-1 flex-col justify-between">
+                <div className="max-w-[315px] font-helvetica text-4xl leading-[52px] font-bold tracking-[-0.18px]">
+                  Monthly Strategy Session
+                </div>
+                <div className="absolute -right-20 -bottom-10">
+                  <Image
+                    src={Circullar}
+                    alt="Circullar Chart Image"
+                    className="h-[325px] w-[325px]"
+                  />
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
       </div>
     </div>
   );
