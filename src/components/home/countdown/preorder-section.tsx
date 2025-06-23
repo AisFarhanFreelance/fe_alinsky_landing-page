f76e0d53c7294638fe3995abef533340f4e8da7c
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-06-23 11:52:45 +0700

    feat: preorder section localization

--- src/components/home/countdown/preorder-section.tsx
+++ src/components/home/countdown/preorder-section.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
 
 const PreorderSection = () => {
@@ -52,13 +53,15 @@ const PreorderSection = () => {
     { value: salesTime.seconds, label: "seconds" },
   ];
 
+  const t = useTranslations("homepage.preOrder");
+
   return (
     <div className="flex justify-center md:my-0">
       <div className="h-fit w-full max-w-[1920px] xl:h-[516px]">
         <div className="mx-8 flex flex-col space-y-6 rounded-4xl bg-alinsky-midnight-blue py-20 text-center font-satoshi text-alinsky-platinum alinsky-2xl:mx-[104px] xl:mx-[104px] xl:space-y-14 xl:py-32 2xl:mx-[344px]">
           <div className="flex flex-col space-y-4">
             <span className="text-xl font-bold capitalize xl:text-[32px]">
-              Pre-Order Closes In
+              {t("preOrderHeading")}
             </span>
             <div className="flex flex-row justify-center space-x-8 text-5xl leading-[100%] font-normal md:text-6xl xl:text-8xl">
               {timeUnits.map((unit, idx) => (
@@ -103,10 +106,10 @@ const PreorderSection = () => {
             }}
             className="text-xl font-bold uppercase xl:text-2xl"
           >
-            <span className="text-alinsky-sunset">HURRY!! </span>
-            <span className="text-alinsky-platinum">Only </span>
+            <span className="text-alinsky-sunset">{t("hurry")}</span>
+            <span className="text-alinsky-platinum">{t("only")}</span>
             <span className="font-bold text-alinsky-beige">20 </span>
-            <span className="text-alinsky-platinum">Left In Stock</span>
+            <span className="text-alinsky-platinum">{t("stockLeft")}</span>
           </motion.div>
         </div>
       </div>
