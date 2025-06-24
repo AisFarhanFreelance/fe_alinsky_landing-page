67ca98b26ef4774e90550ff68f451e7908657a20
Ais Farhan <aisfarhan@Aiss-MacBook-Air.local>
2025-06-24 14:17:51 +0700

    fix: pricing plan

--- src/components/pricing/pricing-plan/pricing-plan-section.tsx
+++ src/components/pricing/pricing-plan/pricing-plan-section.tsx
@@ -1,55 +1,86 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { Variants, motion, useAnimation } from "framer-motion";
 import { useTranslations } from "next-intl";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 import PricingPlanCard from "./pricing-plan-card";
 
 const PricingPlanSection = () => {
   const t = useTranslations("homepage.pricingPlan");
 
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    threshold: 0.2,
+    triggerOnce: true,
+  });
+
+  useEffect(() => {
+    if (inView) controls.start("visible");
+  }, [inView, controls]);
+
+  const containerVariants: Variants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+        delayChildren: 0.3,
+      },
+    },
+  };
+
+  const itemVariants: Variants = {
+    hidden: { y: 30, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+  };
+
   return (
-    <div className="relative w-full pb-16">
+    <motion.section
+      ref={ref}
+      initial="hidden"
+      animate={controls}
+      variants={containerVariants}
+      className="relative w-full pb-16"
+    >
       <div className="flex justify-center">
         <div className="w-full max-w-[1920px] px-8 md:px-14 xl:px-[104px] 2xl:px-[344px]">
-          <div className="mt-[108px] space-y-16">
+          <motion.div
+            variants={containerVariants}
+            className="mt-[108px] space-y-16"
+          >
             <motion.div
+              variants={itemVariants}
               className="space-y-2 text-center capitalize"
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.6 }}
             >
-              <motion.div
-                className="font-helvetica text-2xl leading-16 font-bold text-alinsky-rich-black lg:text-5xl"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.2, duration: 0.8 }}
-              >
+              <div className="font-helvetica text-2xl leading-16 font-bold text-alinsky-rich-black lg:text-5xl">
                 {t("title")}
-              </motion.div>
-              <motion.div
-                className="font-satoshi text-sm font-medium text-alinsky-dim-gray lg:text-xl"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.4, duration: 0.8 }}
-              >
-                {t("description")}
+              </div>
+              <div className="font-satoshi text-sm font-medium text-alinsky-dim-gray lg:text-xl">
+                {t("description")}{" "}
                 <span className="italic">{t("descriptionHighlight")}</span>
-              </motion.div>
+              </div>
             </motion.div>
 
             <motion.div
+              variants={itemVariants}
               className="w-full overflow-x-visible"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 0.6, duration: 0.8 }}
             >
               <PricingPlanCard />
             </motion.div>
-          </div>
+          </motion.div>
         </div>
       </div>
-    </div>
+    </motion.section>
   );
 };
 
