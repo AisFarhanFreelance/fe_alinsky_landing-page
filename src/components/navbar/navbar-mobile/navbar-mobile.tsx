070dac1bb9652a04d1145b410c5b4dc80089a788
Ais Farhan <aisfarhan@Aiss-MacBook-Air.local>
2025-07-01 15:04:42 +0700

    fix: navbar mobile

--- src/components/navbar/navbar-mobile/navbar-mobile.tsx
+++ src/components/navbar/navbar-mobile/navbar-mobile.tsx
@@ -1,7 +1,8 @@
 import { usePathname } from "@/i18n/navigation";
-import { ArrowDown2, HamburgerMenu } from "iconsax-reactjs";
+import { ArrowDown2, CloseSquare, HamburgerMenu } from "iconsax-reactjs";
 import { useLocale, useTranslations } from "next-intl";
 import Image from "next/image";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -17,50 +18,51 @@ import USFlagIcon from "/public/assets/flags/united-states-flag.svg";
 const NavbarMobile = () => {
   const currentLocale = useLocale();
   const pathname = usePathname();
+  const t = useTranslations("navbar");
+  const flagIcon = currentLocale === "id" ? IDFlagIcon : USFlagIcon;
+
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleChange = (newLocale: string) => {
     if (newLocale === currentLocale) return;
-
     const pathSegments = pathname.split("/");
     pathSegments[1] = newLocale;
-
     const newUrl = pathSegments.join("/");
     globalThis.location.href = newUrl;
   };
 
-  const t = useTranslations("navbar");
-
-  const flagIcon = currentLocale === "id" ? IDFlagIcon : USFlagIcon;
-
   return (
-    <DropdownMenu>
+    <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
       <DropdownMenuTrigger asChild>
-        <HamburgerMenu size="32" color="#000000" />
+        <div
+          className="cursor-pointer rounded-[12px] bg-white p-2"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? (
+            <CloseSquare size="32" color="#000000" />
+          ) : (
+            <HamburgerMenu size="32" color="#000000" />
+          )}
+        </div>
       </DropdownMenuTrigger>
+
       <DropdownMenuContent
         align="end"
         className="mx-8 w-[calc(100vw-64px)] bg-[#FAFAFA] p-4 text-[#616161] sm:mx-0 sm:w-sm"
       >
-        <DropdownMenuItem asChild className="mb-6">
-          <a href="#home">{t("home")}</a>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild className="mb-6">
-          <a href="#pricing">{t("pricing")}</a>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild className="mb-6">
-          <a href="#features">{t("features")}</a>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild className="mb-6">
-          <a href="#contact">{t("contact")}</a>
-        </DropdownMenuItem>
+        {["home", "pricing", "features", "contact"].map((item) => (
+          <DropdownMenuItem asChild className="mb-6" key={item}>
+            <a href={`#${item}`}>{t(item)}</a>
+          </DropdownMenuItem>
+        ))}
 
         <DropdownMenuItem>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button className="flex items-center gap-x-2 rounded-full bg-alinsky-rich-black px-4 font-helvetica text-[15px] text-alinsky-white hover:bg-alinsky-rich-black/90 ">
+              <Button className="flex items-center gap-x-2 rounded-full bg-alinsky-rich-black px-4 font-helvetica text-[15px] text-alinsky-white hover:bg-alinsky-rich-black/90">
                 <Image
                   src={flagIcon}
-                  alt="English"
+                  alt="Language"
                   className="h-6 w-6 rounded-full"
                 />
                 <span>{currentLocale.toUpperCase()}</span>
