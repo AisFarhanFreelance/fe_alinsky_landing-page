de154fa4b1e904f7758f6406442a71d4fdcd4cfa
Ais Farhan <aisfarhan@Aiss-MacBook-Air.local>
2025-07-01 15:17:13 +0700

    fix: pricing-footer is not center

--- src/components/footer/pricing-footer.tsx
+++ src/components/footer/pricing-footer.tsx
@@ -11,7 +11,7 @@ const PricingFooter = () => {
         whileInView={{ opacity: 1, y: 0, scale: 1 }}
         viewport={{ once: true, margin: "-20% 0px -20% 0px" }}
         transition={{ duration: 0.6, ease: [0.175, 0.885, 0.32, 1.275] }}
-        className="mx-8 w-fit items-center justify-center rounded-full bg-alinsky-midnight-blue px-14 py-5 font-satoshi text-base font-normal text-alinsky-white alinsky-2xl:mx-[104px] md:mx-14 xl:mx-[104px] xl:flex xl:text-2xl"
+        className="mx-8 w-fit  items-center justify-center self-center rounded-full bg-alinsky-midnight-blue px-14 py-5 text-center font-satoshi text-base font-normal text-alinsky-white alinsky-2xl:mx-[104px] md:mx-14 xl:mx-[104px] xl:flex xl:text-2xl"
       >
         {t("headline")}
         <b>
